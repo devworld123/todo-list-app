ce7fe6d66c1711a2d328f2ae4a33a2faa58c65f3
devworld123 <anjumazhuvanoor@gmail.com>
2024-09-09 17:22:30 +0530

    modified task class and main class methods

--- src/com/ust/tolist/application/Task.java
+++ src/com/ust/tolist/application/Task.java
@@ -3,13 +3,18 @@ package com.ust.tolist.application;
 public class Task {
 	private String description;
     private String dueDate;
+    private int priority;
 
-    public Task(String description, String dueDate) {
-        this.description = description;
-        this.dueDate = dueDate;
-    }
+   
 
-    public String getDescription() {
+    public Task(String description, String dueDate, int priority) {
+		super();
+		this.description = description;
+		this.dueDate = dueDate;
+		this.priority = priority;
+	}
+
+	public String getDescription() {
         return description;
     }
 
@@ -24,10 +29,17 @@ public class Task {
     public void setDueDate(String dueDate) {
         this.dueDate = dueDate;
     }
+    public int getPriority() {
+        return priority;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
 
     @Override
-    public String toString() {
-        return "Task: " + description + ", Due Date: " + dueDate;
-    }
+	public String toString() {
+		return "Task [description=" + description + ", dueDate=" + dueDate + ", priority=" + priority + "]";
+	}
 
 }
--- src/com/ust/tolist/application/ToDoListApplication.java
+++ src/com/ust/tolist/application/ToDoListApplication.java
@@ -3,19 +3,30 @@ package com.ust.tolist.application;
 import java.util.Scanner;
 
 public class ToDoListApplication {
-	
+
 	private Task[] tasks;
-    private int taskCount;
-    
+	private int taskCount;
+
 	public ToDoListApplication(int size) {
-        tasks = new Task[size];
-        taskCount = 0;
+		tasks = new Task[size];
+		taskCount = 0;
 	}
-	
+
+	public void addTask(String description, String dueDate, int priority) {
+		if (taskCount < tasks.length) {
+			tasks[taskCount++] = new Task(description, dueDate, priority);
+			System.out.println("Task added with priority.");
+		} else {
+			System.out.println("Task list is full.");
+		}
+	}
+
 	public static void main(String[] args) {
 		ToDoListApplication toDoList = new ToDoListApplication(10);
 		Scanner scanner = new Scanner(System.in);
 		boolean exit = false;
+		
+		toDoList.addTask("Complete project", "2024-09-10", 1);
 	}
 
 }
