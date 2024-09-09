6ffd90c0c8299ac5e196f4b04bf91ec7343d306c
devworld123 <anjumazhuvanoor@gmail.com>
2024-09-09 17:16:34 +0530

    Added remove task method

--- src/com/ust/tolist/application/ToDoListApplication.java
+++ src/com/ust/tolist/application/ToDoListApplication.java
@@ -12,10 +12,23 @@ public class ToDoListApplication {
         taskCount = 0;
 	}
 	
+	public void removeTask(int index) {
+	    if (index >= 0 && index < taskCount) {
+	        for (int i = index; i < taskCount - 1; i++) {
+	            tasks[i] = tasks[i + 1];
+	        }
+	        tasks[--taskCount] = null;
+	        System.out.println("Task removed.");
+	    } else {
+	        System.out.println("Invalid index.");
+	    }
+	}
 	public static void main(String[] args) {
 		ToDoListApplication toDoList = new ToDoListApplication(10);
 		Scanner scanner = new Scanner(System.in);
 		boolean exit = false;
+		
+		toDoList.removeTask(0);
 	}
 
 }
