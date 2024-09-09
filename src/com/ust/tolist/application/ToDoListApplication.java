5502475778b4f883d378aa96cabc0d4734392418
devworld123 <anjumazhuvanoor@gmail.com>
2024-09-09 17:13:52 +0530

    addTask method added

--- src/com/ust/tolist/application/ToDoListApplication.java
+++ src/com/ust/tolist/application/ToDoListApplication.java
@@ -11,11 +11,20 @@ public class ToDoListApplication {
         tasks = new Task[size];
         taskCount = 0;
 	}
-	
+	public void addTask(String description, String dueDate) {
+	    if (taskCount < tasks.length) {
+	        tasks[taskCount++] = new Task(description, dueDate);
+	        System.out.println("Task added.");
+	    } else {
+	        System.out.println("Task list is full.");
+	    }
+	}
 	public static void main(String[] args) {
 		ToDoListApplication toDoList = new ToDoListApplication(10);
 		Scanner scanner = new Scanner(System.in);
 		boolean exit = false;
+		
+		toDoList.addTask("Complete project", "2024-09-10");
 	}
 
 }
