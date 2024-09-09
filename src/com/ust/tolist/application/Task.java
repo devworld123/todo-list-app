39799a48bcca32a352c0d062988bb1945ed9d8be
devworld123 <anjumazhuvanoor@gmail.com>
2024-09-09 09:55:21 +0530

    task class properties ans methods added

--- src/com/ust/tolist/application/Task.java
+++ src/com/ust/tolist/application/Task.java
@@ -1,5 +1,33 @@
 package com.ust.tolist.application;
 
 public class Task {
+	private String description;
+    private String dueDate;
+
+    public Task(String description, String dueDate) {
+        this.description = description;
+        this.dueDate = dueDate;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(String dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Task: " + description + ", Due Date: " + dueDate;
+    }
 
 }
