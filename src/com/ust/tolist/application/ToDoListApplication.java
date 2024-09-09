1c45998bd8ddffce4dd2565776de0490906f4902
devworld123 <anjumazhuvanoor@gmail.com>
2024-09-09 17:09:55 +0530

    main update

--- src/com/ust/tolist/application/ToDoListApplication.java
+++ src/com/ust/tolist/application/ToDoListApplication.java
@@ -1,5 +1,21 @@
 package com.ust.tolist.application;
 
+import java.util.Scanner;
+
 public class ToDoListApplication {
+	
+	private Task[] tasks;
+    private int taskCount;
+    
+	public ToDoListApplication(int size) {
+        tasks = new Task[size];
+        taskCount = 0;
+	}
+	
+	public static void main(String[] args) {
+		ToDoListApplication toDoList = new ToDoListApplication(10);
+		Scanner scanner = new Scanner(System.in);
+		boolean exit = false;
+	}
 
 }
